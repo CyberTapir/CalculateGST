335b87ccacbbe93b398259350a2c7de890a1c140
CyberTapir <callumtbennett@gmail.com>
2023-05-15 09:40:23 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lstOutput = New ListBox()
+        SuspendLayout()
+        ' 
+        ' lstOutput
+        ' 
+        lstOutput.FormattingEnabled = True
+        lstOutput.ItemHeight = 15
+        lstOutput.Location = New Point(12, 4)
+        lstOutput.Name = "lstOutput"
+        lstOutput.Size = New Size(274, 814)
+        lstOutput.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(298, 830)
+        Controls.Add(lstOutput)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents lstOutput As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        driver()
+    End Sub
+    Private Function addGST(excGST As Single) As Single
+        Const GST_rate As Integer = 10
+        Dim incGST As Single
+        incGST = excGST * (1 + (GST_rate / 100))
+        Return incGST
+    End Function
+    Private Sub driver()
+        For i = 1.0 To 10.0 Step 0.01
+            lstOutput.Items.Add("Input price: " & FormatCurrency(i) & " Output price: " & FormatCurrency(addGST(i)))
+        Next i
+    End Sub
+End Class
