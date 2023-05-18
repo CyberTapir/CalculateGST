07fddd2e73fe1c4684cd57b12b9aecb691273c6a
CyberTapir <callumtbennett@gmail.com>
2023-05-18 11:58:02 +1000

    x

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,26 +23,55 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         lstOutput = New ListBox()
+        rdoVat1 = New RadioButton()
+        rdoVat2 = New RadioButton()
         SuspendLayout()
         ' 
         ' lstOutput
         ' 
         lstOutput.FormattingEnabled = True
         lstOutput.ItemHeight = 15
-        lstOutput.Location = New Point(12, 4)
+        lstOutput.Location = New Point(247, 4)
         lstOutput.Name = "lstOutput"
-        lstOutput.Size = New Size(274, 814)
+        lstOutput.Size = New Size(400, 814)
         lstOutput.TabIndex = 1
+        ' 
+        ' rdoVat1
+        ' 
+        rdoVat1.AutoSize = True
+        rdoVat1.Location = New Point(37, 79)
+        rdoVat1.Name = "rdoVat1"
+        rdoVat1.Size = New Size(41, 19)
+        rdoVat1.TabIndex = 2
+        rdoVat1.TabStop = True
+        rdoVat1.Text = "1%"
+        rdoVat1.UseVisualStyleBackColor = True
+        ' 
+        ' rdoVat2
+        ' 
+        rdoVat2.AutoSize = True
+        rdoVat2.Location = New Point(37, 104)
+        rdoVat2.Name = "rdoVat2"
+        rdoVat2.Size = New Size(41, 19)
+        rdoVat2.TabIndex = 3
+        rdoVat2.TabStop = True
+        rdoVat2.Text = "8%"
+        rdoVat2.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(298, 830)
+        ClientSize = New Size(704, 830)
+        Controls.Add(rdoVat2)
+        Controls.Add(rdoVat1)
         Controls.Add(lstOutput)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
     Friend WithEvents lstOutput As ListBox
+    Friend WithEvents rdoVat1 As RadioButton
+    Friend WithEvents rdoVat2 As RadioButton
 End Class
--- Form1.vb
+++ Form1.vb
@@ -10,7 +10,15 @@
     End Function
     Private Sub driver()
         For i = 1.0 To 10.0 Step 0.01
-            lstOutput.Items.Add("Input price: " & FormatCurrency(i) & " Output price: " & FormatCurrency(addGST(i)))
+            lstOutput.Items.Add("Input price: " & FormatCurrency(i) & " Output price: " & FormatCurrency(addGST(i))
         Next i
     End Sub
 End Class
+
+'This function 
+'Private Function addGST(excGST As Single) As Single
+'    Const GST_rate As Integer = 10
+'    Dim incGST As Single
+'    incGST = excGST * (1 + (GST_rate / 100))
+'    Return incGST
+'End Function
